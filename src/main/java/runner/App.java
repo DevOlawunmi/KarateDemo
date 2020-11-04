bee58b8035de3923a8cf732cdfcb22d918950cd5
Ola <olawunmi263@yahoo.com>
2020-11-04 03:58:13 +0000

    Test runner folder updated

--- src/main/java/runner/App.java
+++ src/main/java/runner/App.java
@@ -1,4 +1,4 @@
-package KarateDemo;
+package runner;
 
 /**
  * Hello world!
--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,11 +1,11 @@
-package KarateDemo;
+package runner;
 
 /*
 The API layer contains the business logic of an application -
 the rules of how users can interact with services, data, or functions of the app.
 The portal has 5 main functionalities: user can submit an LBA instruction or a Full Instruction, the user can convert an
 LBA Instruction to Full, they can send LBA chasers and they can search for existing cases.
-For these processes to run correctly, we send API calls  to our database backend and to other web services like the DVLA API
+For these processes to run correctly, we send API calls  to our backend and to other web services like the DVLA API
 to get details of the vehicles involved in the incident,
 Address Lookup API, and Insurers API. In addition to this, ping authenticates users trying to login to the portal so that's
 another API call being made from the portal.
