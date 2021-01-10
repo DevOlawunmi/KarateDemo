63c20f57d687133510f26d0ba5dce25d82374679
Ola <olawunmi263@yahoo.com>
2021-01-10 05:39:57 +0000

    Notes added to test runner

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -10,14 +10,15 @@ to get details of the vehicles involved in the incident,
 Address Lookup API, and Insurers API. In addition to this, ping authenticates users trying to login to the portal so that's
 another API call being made from the portal.
  */
+import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit4.Karate;
 import cucumber.api.CucumberOptions;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 
-
+// I still need to figure out how to point to the features folder instead of listing ind files
 @RunWith(Karate.class)
-@CucumberOptions(
+@KarateOptions(
         features = {"src/test/java/features/getClient.feature", "src/test/java/features/getVehicleDetails.feature", "src/test/java/features/lookupAddress.feature", "src/test/java/features/searchCases.feature"}
 )
 
